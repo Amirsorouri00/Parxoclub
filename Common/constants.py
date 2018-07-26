e25e94cc444e33ef8c42565ccc6159be1a201916
Amir Hossein <amirsorouri26@gmail.com>
2018-07-26 10:53:59 +0430

    CACHE_TTL added to the constants variable

--- Common/constants.py
+++ Common/constants.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
 CHAT_CONSUMER_PREFIX_GROUP_NAME = 'chat_%s'
 USER_STRING = 'USER-PRIVATE-KEY'
 ROOM_STRING = 'ROOM-PRIVATE-KEY'
 USER_CONTACT_HAVE_ROOM = 'UserHaveRoomWithContact'
 USER_EXIST_IN_ROOM = 'UserExistInTheRoom'
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
