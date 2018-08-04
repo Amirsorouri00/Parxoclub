8abf0131d6a2c08dff759e6e4d4c995947097036
Amir Hossein <amirsorouri26@gmail.com>
2018-08-04 19:43:04 +0430

    search using ajax implemented blob blob

--- PraxoClub/settings.py
+++ PraxoClub/settings.py
@@ -45,16 +45,17 @@ INSTALLED_APPS = [
     'Common',
     'Log',
     'rest_framework', # Django Rest framework app
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
-    #'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 #Authentication backends
