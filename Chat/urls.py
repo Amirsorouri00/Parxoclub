6cdfd6b050cdc7c5473f42e940bcc67b59f3f93a
Amir Hossein <amirsorouri26@gmail.com>
2018-08-26 19:25:39 +0430

    adding existing pages into index.html href links

--- Chat/urls.py
+++ Chat/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='chat'),
     path('userchats/<int:userId>/', views.UserChats, name = 'reactWholeChat'),
     path('panel/', views.Chat, name = 'chat'),
     url(r'^(?P<privatekey>[^/]+)/(?P<senderprivatekey>[^/]+)$', views.room, name='room'),
